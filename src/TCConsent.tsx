a4ac54b4f31f5a8b7cf20cc8311c34850992f474
Abdelhakim SAID <abdelhakim.said@commandersact.com>
2024-01-15 14:56:49 +0100

    update package scoped name, remove Hard dependencies & fix js imports

--- src/TCConsent.tsx
+++ src/TCConsent.tsx
@@ -1,5 +1,5 @@
 import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
-import { TCUserInstance } from 'tccore-react-native'; 
+import { TCUserInstance } from '@commandersact/tccore-react-native'; 
 
 const LINKING_ERROR =
   `The package 'tcconsent-react-native' doesn't seem to be linked. Make sure: \n\n` +
