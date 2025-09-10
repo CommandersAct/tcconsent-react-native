e177cbd950d66552c1d7c2d6898603fd4c822a61
Commanders Act <jean.julien.zeil@commandersact.com>
2025-09-10 17:40:11 +0200

    New release version: 1.3.9

--- android/src/main/java/com/tcconsentreactnative/TCConsentReactNativeGenerated.java
+++ android/src/main/java/com/tcconsentreactnative/TCConsentReactNativeGenerated.java
@@ -6,5 +6,5 @@ package com.tcconsentreactnative;
 
 public class TCConsentReactNativeGenerated
 {
-    public static final String version = "1.3.8";
+    public static final String version = "1.3.9";
 }
